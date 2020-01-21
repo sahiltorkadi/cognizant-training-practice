c6dbec953e56aa0b8d6efe41e9b42f4eaff1efae
sahiltorkadi <32193690+sahiltorkadi@users.noreply.github.com>
2020-01-21 15:33:04 +0530

    Add files via upload

---
+++ cal-script.js
@@ -0,0 +1,21 @@
+<script> 
+var inputX = document.getElementById('x'),
+    inputY = document.getElementById('y'),
+    output = document.getElementById('output');
+
+    var showOutput = function(value) {
+    output.innerHTML = value;
+}
+
+var operation = function(operationName) {
+    var x = getInputs(inputX),
+        y = getInputs(inputY);
+
+    if (isNaN(x) || isNaN(y)) {
+     
+        return;
+    }
+
+    
+};
+      </script> 
