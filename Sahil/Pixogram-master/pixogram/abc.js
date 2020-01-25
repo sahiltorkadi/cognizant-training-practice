abae4d0d33655a54add4e62a5cf9ed509d609115
sahiltorkadi <32193690+sahiltorkadi@users.noreply.github.com>
2020-01-25 17:28:52 +0530

    Add files via upload

---
+++ Sahil/Pixogram-master/pixogram/abc.js
@@ -0,0 +1,31 @@
+const one= function()
+{
+    console.log('one')
+}
+const two= ()=>
+{
+    console.log('two')
+}
+const three =()=>
+{
+    console.log('three')
+}
+const four= function()
+{
+    console.log('four')
+}
+five = function()
+{
+    console.log('five')
+}
+const zero=()=>
+{
+    console.log('zero')
+    one()
+    setTimeout(two,2000)
+    three()
+    four()
+    five()
+
+}
+zero()
