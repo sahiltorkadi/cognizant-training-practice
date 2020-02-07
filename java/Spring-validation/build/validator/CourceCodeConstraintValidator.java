c7af819f4e81ddeb2e6ed0bebbeee276ede122f6
sahiltorkadi <32193690+sahiltorkadi@users.noreply.github.com>
2020-02-07 17:51:51 +0530

    Add files via upload

---
+++ java/Spring-validation/build/validator/CourceCodeConstraintValidator.java
@@ -0,0 +1,23 @@
+package com.cts.training.spring.web.validator;
+
+
+public class CourseCodeConstriantValidator implements ConstraintValidator<CourseCode, String>{
+
+	private String expectedCode;
+	// method that will provide code expected
+	@Override
+	public void initialize(CourseCode constraintAnnotation) {
+		this.expectedCode = constraintAnnotation.code();
+	}
+	
+	// if data is good return : true else false
+	// actualData : data entered by user
+	@Override
+	public boolean isValid(String actualData, ConstraintValidatorContext arg1) {
+		// TODO Auto-generated method stub
+		if(actualData.startsWith(this.expectedCode))
+			return true;
+		return false;
+	}
+
+}
